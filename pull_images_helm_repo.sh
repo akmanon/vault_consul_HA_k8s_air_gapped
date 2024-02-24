2228f2032f6aec54c5b38870a8eb9f3c40361cb6
Ashish Kumar <kashish979@gmail.com>
2024-02-24 12:38:33 +0530

    Pull images and save it

---
+++ pull_images_helm_repo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#Add and fetch helm repo for vault and consul
+helm repo add hashicorp https://helm.releases.hashicorp.com
+helm fetch hashicorp/consul --version 0.28.0
+helm fetch hashicorp/vault --version 0.8.0
+
+:> image_out.txt
+mkdir -p image_out
+
+images=(
+    "hashicorp/vault-k8s:1.3.1"
+    "hashicorp/vault:1.15.2"
+    "hashicorp/consul:1.9.1"
+)
+
+for image in "${images[@]}"; do
+    # Pull the image from the registry
+    docker pull "$image"
+
+    # Save the image to a tar file on the local disk
+    image_name=$(echo "$image" | sed 's|/|_|g' | sed 's/:/_/g')
+    docker save -o "${image_name}.tar" "$image"
+    echo "${image_name}.tar" >> image_out.txt
+done
