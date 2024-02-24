a3b69a36fa4b8612201a3b86627ac7f245361ab1
Ashish Kumar <kashish979@gmail.com>
2024-02-24 13:41:34 +0530

    create Registry and deploy images

--- create_registry_deploy_images.sh
+++ create_registry_deploy_images.sh
@@ -5,9 +5,6 @@ IMAGE_DIR="image_out"
 docker load -i ./image_out/registry_2.8.2.tar
 docker run -d -p 5000:5000 --restart=always --name registry registry:2.8.2
 
-
-#hashicorp_consul_1.9.1.tar  hashicorp_vault-k8s_1.3.1.tar  hashicorp_vault_1.15.2.tar
-
 cat "image_list.txt" | while IFS= read -r info_image; do
     image_name="$(echo $info_image | awk '{print $1}')"
     image="$(echo $info_image | awk '{print $2}')"
