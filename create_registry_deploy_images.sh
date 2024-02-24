8e065840224c3ea4afeb811edbab937b6377d5dc
Ashish Kumar <kashish979@gmail.com>
2024-02-24 13:18:46 +0530

    create Registry and deploy images

--- create_registry_deploy_images.sh
+++ create_registry_deploy_images.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-docker load -i registry_2.8.2.tar
+docker load -i ./image_out/registry_2.8.2.tar
 docker run -d -p 5000:5000 --restart=always --name registry registry:2.8.2
+
+
+#hashicorp_consul_1.9.1.tar  hashicorp_vault-k8s_1.3.1.tar  hashicorp_vault_1.15.2.tar
+
+for info_image in $(cat images_out.txt); do
+    $image_name=$(echo $info_image | awk '{print $1}')
+    $image_name=$(echo $info_image | awk '{print $2}')
+    docker load -i $image
+    docker tag $image_name localhost:5000/$image_name
+    docker push localhost:5000/$image_name
--- pull_images_and_helm_repo.sh
+++ pull_images_and_helm_repo.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-:> image_out.txt
+#Dir for saving images and helm repos
 mkdir -p image_out
 mkdir -p repo_out
+:> image_list.txt
 
 #Add and fetch helm repo for vault and consul
 helm repo add hashicorp https://helm.releases.hashicorp.com
@@ -10,6 +11,7 @@ helm fetch hashicorp/consul --version 0.28.0 -d ./repo_out
 helm fetch hashicorp/vault --version 0.8.0 -d ./repo_out
 
 images=(
+    "registry:2.8.2"
     "hashicorp/vault-k8s:1.3.1"
     "hashicorp/vault:1.15.2"
     "hashicorp/consul:1.9.1"
@@ -22,5 +24,5 @@ for image in "${images[@]}"; do
     # Save the image to a tar file on the local disk
     image_name=$(echo "$image" | sed 's|/|_|g' | sed 's/:/_/g')
     docker save -o "./image_out/${image_name}.tar" "$image"
-    echo "${image_name}.tar" >> image_out.txt
+    echo "${image} ${image_name}.tar"
 done
